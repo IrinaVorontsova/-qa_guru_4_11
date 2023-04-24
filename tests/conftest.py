adcdc4dd0391252a229cc57e257729d8ba7d7a91
Irina Vorontsova <irina.n.vorontsova@gmail.com>
2023-04-25 00:03:46 +0300

    mini fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,10 @@
 import pytest
 import os
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from dotenv import load_dotenv
 
 from utils import attach
-
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
