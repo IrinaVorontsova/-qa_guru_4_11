c838a3abb52fcc10f6f887d9054acc5bd3cae0ef
Irina Vorontsova <irina.n.vorontsova@gmail.com>
2023-04-30 18:38:15 +0300

    fixed called browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,25 +18,13 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture(scope='class', autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='class')
-def setup_browser(set_driver):
-    # browser.config.driver = set_driver
-    setup_browser = Browser(Config(set_driver))
-    yield setup_browser
-
-    attach.add_html(setup_browser)
-    attach.add_screenshot(setup_browser)
-    attach.add_logs(setup_browser)
-    attach.add_video(setup_browser)
-    setup_browser.quit()
-
-@pytest.fixture(scope='class')
-def set_driver(request):
+@pytest.fixture(scope='function')
+def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
@@ -53,7 +41,16 @@ def set_driver(request):
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
 
-    return webdriver.Remote(
+    driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
+    browser = Browser(Config(driver))
+
+    yield browser
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
